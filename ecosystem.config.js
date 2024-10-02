5e1e4db74f107bd47bea4a7170e3b3f392866c19
南極雄 <70533278+mcg25035@users.noreply.github.com>
2024-10-03 05:56:51 +0800

    implement fcu signin api

---
+++ ecosystem.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  apps : [{
+    name: "turtle",
+    script: "./index.js"
+  }]
+}
--- index.js
+++ index.js
@@ -5,69 +5,34 @@ const path = require("path")
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const Discord = require("discord.js")
-const secretData = require('./.secret.json');
-
-const client = new Discord.Client({intents: [
-    Discord.GatewayIntentBits.Guilds,
-    Discord.GatewayIntentBits.GuildMessages    
-]})
-
-client.login(secretData.token)
-lastDiscordMessageNotified = -1;
-
-client.on('ready', async () => {
-    const channel = client.channels.cache.get(secretData.channel)
-    await channel.send('烏龜烏龜翹系統已啟動');
-    
-});
-
-
-var qrcodeData = ""
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-
-app.get('/scanner', (req, res) => {
-    res.sendFile(__dirname + '/scanner.html');
-});
 app.get('/qrcode', (req, res) => {
     res.sendFile(__dirname + '/qrcode.html');
 });
 
+app.get('/scanner', (req, res) => {
+    res.sendFile(__dirname + '/scanner.html');
+});
 
+// Handle Socket.io connections
 io.on('connection', (socket) => {
     console.log('A user connected');
 
+    // Handle messages from clients
     socket.on('QRCodeDetected', (msg) => {
-        if (msg != qrcodeData){
-            qrcodeData = msg
-            socket.broadcast.emit('refresh', msg);
-        }
+        console.log(msg)
+        socket.broadcast.emit('QRCodeDetected', msg);
     });
 
-    socket.on('ScannerConntected', async ()=>{
-        socket.broadcast.emit('notify');
-        var now = Date.now();
-        if (now - lastDiscordMessageNotified < 1000*60*10) return;
-        lastDiscordMessageNotified = now;
-        const channel = client.channels.cache.get(secretData.channel)
-        await channel.send(
-            `<@&${secretData.role}>\n`+
-            '各位烏龜大家好\n'+
-            '等等我們即將開始課程的點名\n'+
-            `敬請各位準備好連上[網站](${secretData.url})\n`+
-            '並打開你的行動逢甲\n'+
-            '我們準備點名'
-        );
-
-    })
-
+    // Handle disconnections
     socket.on('disconnect', () => {
         console.log('A user disconnected');
     });
 });
 
+// Start the server on port 3000
 server.listen(3000, () => {
     console.log('Server listening on http://localhost:3000');
 });
